d938e846aeeed34292a5e45b75a464df598273f8
Anton Sukhinets <antonvezde1@gmail.com>
2022-09-03 23:31:03 +0200

    first commit

---
+++ binary-tree.py
@@ -0,0 +1,34 @@
+class BinaryTree:
+
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert_node(self, data):
+        if data == self.data:  # if binary tree already has an element -> return
+            return
+        elif data < self.data:
+            # if element < than data -> add left node
+            if self.left:
+                self.left.insert_node(data)
+            else:
+                self.left = BinaryTree(data)
+        else:
+            # if element > than data -> add right node
+            if self.right:
+                self.right.insert_node(data)
+            else:
+                self.right = BinaryTree(data)
+
+    def in_order_traversal(self): # left -> root -> right
+        tree_data = list()
+        if self.left:
+            tree_data += self.left.in_order_traversal()
+        tree_data.append(self.data)
+        if self.right:
+            tree_data += self.right.in_order_traversal()
+        return tree_data
+
+node = BinaryTree(3)
+print(node.data)
